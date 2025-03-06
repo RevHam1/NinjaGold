1c9515c12911b9e7ee88ec9cf05ca71c41293d15
RevHam1 <36148347+RevHam1@users.noreply.github.com>
2025-03-06 01:10:48 -0600

    Implemented You’ve searched to select the 4 for a total & game termination

--- ninja_gold_app/views.py
+++ ninja_gold_app/views.py
@@ -8,41 +8,55 @@ def index(request):
         request.session['gold'] = 0
     if 'activites' not in request.session:
         request.session['activites'] = []
+    if 'used_buildings' not in request.session:
+        request.session['used_buildings'] = []
     return render(request, 'index.html')
 
 
 def process_money(request):
     print("The form has been submitted!")
     print(request.POST)
-    if request.POST['building'] == 'farm':
+    building = request.POST['building']
+
+    if building in request.session['used_buildings']:
+        request.session['activites'].append(
+            f"You have already used the {building.capitalize()}! Choose another building.")
+        return redirect('/')
+
+    if building == 'farm':
         num = random.randint(10, 20)
         request.session['gold'] += num
         request.session['activites'].append(
-            "You Found " + str(num) + " ounces of Gold on a Farm! Yay!")
+            f"You Found {num} ounces of Gold on a Farm! Yay!")
 
-    elif request.POST['building'] == 'cave':
+    elif building == 'cave':
         num = random.randint(5, 10)
         request.session['gold'] += num
         request.session['activites'].append(
-            "You Discovered " + str(num) + " ounces of Gold in a Cave! Yay!")
+            f"You Discovered {num} ounces of Gold in a Cave! Yay!")
 
-    elif request.POST['building'] == 'house':
+    elif building == 'house':
         num = random.randint(2, 5)
         request.session['gold'] += num
         request.session['activites'].append(
-            "You Stolen " + str(num) + " ounces of Gold from a House! Yay!")
+            f"You Stolen {num} ounces of Gold from a House! Yay!")
 
-    elif request.POST['building'] == 'casino':
+    elif building == 'casino':
         num = random.randint(-50, 50)
         request.session['gold'] += num
         if num > 0:
             request.session['activites'].append(
-                "You Won " + str(num) + " ounces of Gold in the Casino! Yay!")
+                f"You Won {num} ounces of Gold at the Casino! Yay!")
         elif num == 0:
             request.session['activites'].append("You earned nothing")
         else:
             request.session['activites'].append(
-                "You Lost " + str(abs(num)) + " ounces of Gold in the Casino! Boooo!!")
+                f"You Lost {abs(num)} ounces of Gold at the Casino! Boooo!!")
+
+    # Mark the building as used
+    request.session['used_buildings'].append(building)
+    request.session.modified = True  # Ensure session is saved
+
     return redirect('/')
 
 

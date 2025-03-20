578992428238b6865b8dcf74574d52c7e1b8bd45
RevHam1 <36148347+RevHam1@users.noreply.github.com>
2025-03-20 05:27:17 -0500

    Testing Sound

--- ninja_gold_app/urls.py
+++ ninja_gold_app/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('', views.index),
     path('process_money', views.process_money),
     path('reset', views.reset),
+    path('ninja_gold/', views.ninja_gold_game, name='ninja_gold_game'),
+    path('reset/', views.reset, name='reset'),
+    path('get_gold_value/', views.get_gold_value, name='get_gold_value'),
 ]

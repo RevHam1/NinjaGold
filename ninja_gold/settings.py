1133587cbd1a57d1b0bc6b87fd4325e39ddd1174
RevHam1 <36148347+RevHam1@users.noreply.github.com>
2025-03-23 21:42:04 -0500

    Made Changes

--- ninja_gold/settings.py
+++ ninja_gold/settings.py
@@ -174,7 +174,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # STATICFILES_DIRS = [BASE_DIR / 'static']
 # STATIC_ROOT = BASE_DIR / 'staticfiles'

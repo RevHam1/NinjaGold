0521d79a354d8b722fd870fd3698fe381f0d8be5
RevHam1 <36148347+RevHam1@users.noreply.github.com>
2025-03-26 00:23:42 -0500

    smiplified

--- ninja_gold_app/urls.py
+++ ninja_gold_app/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('process_money', views.process_money),
-    path('reset', views.reset),
-    path('ninja_gold/', views.ninja_gold_game, name='ninja_gold_game'),
-    path('reset/', views.reset, name='reset'),
-    path('get_gold_value/', views.get_gold_value, name='get_gold_value'),
+    # path('process_money', views.process_money),
+    # path('reset', views.reset),
+    # path('ninja_gold/', views.ninja_gold_game, name='ninja_gold_game'),
+    # path('reset/', views.reset, name='reset'),
+    # path('get_gold_value/', views.get_gold_value, name='get_gold_value'),
 
     # path('supabasedata/', views.your_view, name='supabasedata'),
 ]
